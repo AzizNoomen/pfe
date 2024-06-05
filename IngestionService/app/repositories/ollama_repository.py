a2b228094c87585700fc0a33c14e9815ce71cce1
testaziz101-admin <aziznoomen1@gmail.com>
2024-06-05 18:28:13 +0100

    added the necessary exceptions for ollama

--- IngestionService/app/repositories/ollama_repository.py
+++ IngestionService/app/repositories/ollama_repository.py
@@ -1,7 +1,7 @@
-# app/repositories/ollama_repository.py
 import os
 import requests
 import json
+from fastapi import HTTPException
 
 BASE_URL = os.environ.get('OLLAMA_HOST', 'http://host.docker.internal:11434')
 
@@ -17,12 +17,10 @@ class OllamaRepository:
                 "context": context,
                 "options": options
             }
-
             payload = {k: v for k, v in payload.items() if v is not None}
 
             with requests.post(url, json=payload, stream=True) as response:
                 response.raise_for_status()
-
                 full_response = ""
 
                 for line in response.iter_lines():
@@ -35,152 +33,88 @@ class OllamaRepository:
                 return full_response
 
         except requests.exceptions.RequestException as e:
-            print(f"An error occurred: {e}")
-            return None
+            raise HTTPException(status_code=500, detail=f"An error occurred: {e}")
 
-    # Create a model from a Modelfile. Use the callback function to override the default handler.
-    def create_model(self, model_name, model_path=None, callback=None):
+    def create_model(self, model_name, model_path=None):
         try:
             url = f"{BASE_URL}/api/create"
             payload = {"name": model_name, "path": model_path}
-            
-            # Making a POST request with the stream parameter set to True to handle streaming responses
+
             with requests.post(url, json=payload, stream=True) as response:
                 response.raise_for_status()
 
-                # Iterating over the response line by line and displaying the status
                 for line in response.iter_lines():
                     if line:
-                        # Parsing each line (JSON chunk) and extracting the status
                         chunk = json.loads(line)
-
-                        if callback:
-                            callback(chunk)
-                        else:
-                            print(f"Status: {chunk.get('status')}")
+                        print(f"Status: {chunk.get('status')}")
 
         except requests.exceptions.RequestException as e:
-            print(f"An error occurred: {e}")
+            raise HTTPException(status_code=500, detail=f"An error occurred: {e}")
 
-    # Pull a model from a the model registry. Cancelled pulls are resumed from where they left off, and multiple
-    # calls to will share the same download progress. Use the callback function to override the default handler.
-    def pull_model(self, model_name, insecure=False, callback=None):
+    def pull_model(self, model_name, insecure=False):
         try:
             url = f"{BASE_URL}/api/pull"
-            payload = {
-                "name": model_name,
-                "insecure": insecure
-            }
+            payload = {"name": model_name, "insecure": insecure}
 
-            # Making a POST request with the stream parameter set to True to handle streaming responses
             with requests.post(url, json=payload, stream=True) as response:
                 response.raise_for_status()
 
-                # Iterating over the response line by line and displaying the details
                 for line in response.iter_lines():
                     if line:
-                        # Parsing each line (JSON chunk) and extracting the details
                         chunk = json.loads(line)
-                        # If a callback function is provided, call it with the chunk
-                        if callback:
-                            callback(chunk)
-                        else:
-                            # Print the status message directly to the console
-                            print(chunk.get('status', ''), end='', flush=True)
-                        
-                        # If there's layer data, you might also want to print that (adjust as necessary)
-                        if 'digest' in chunk:
-                            print(f" - Digest: {chunk['digest']}", end='', flush=True)
-                            print(f" - Total: {chunk['total']}", end='', flush=True)
-                        if 'Compeletd' in chunk:
-                            print(f" - Completed: {chunk['completed']}", end='\n', flush=True)
+                        if 'Completed' in chunk:
+                            print(f" - Completed: {chunk['Completed']}")
                         else:
                             print()
 
         except requests.exceptions.RequestException as e:
-            print(f"An error occurred: {e}")
+            raise HTTPException(status_code=500, detail=f"An error occurred: {e}")
 
-    # Push a model to the model registry. Use the callback function to override the default handler.
-    def push_model(self, model_name, insecure=False, callback=None):
+    def push_model(self, model_name, insecure=False):
         try:
             url = f"{BASE_URL}/api/push"
-            payload = {
-                "name": model_name,
-                "insecure": insecure
-            }
+            payload = {"name": model_name, "insecure": insecure}
 
-            # Making a POST request with the stream parameter set to True to handle streaming responses
             with requests.post(url, json=payload, stream=True) as response:
                 response.raise_for_status()
 
-                # Iterating over the response line by line and displaying the details
                 for line in response.iter_lines():
                     if line:
-                        # Parsing each line (JSON chunk) and extracting the details
                         chunk = json.loads(line)
+                        print(f"Status: {chunk.get('status')}")
 
-                        # If a callback function is provided, call it with the chunk
-                        if callback:
-                            callback(chunk)
-                        else:
-                            # Print the status message directly to the console
-                            print(chunk.get('status', ''), end='', flush=True)
-                        
-                        # If there's layer data, you might also want to print that (adjust as necessary)
-                        if 'digest' in chunk:
-                            print(f" - Digest: {chunk['digest']}", end='', flush=True)
-                            print(f" - Total: {chunk['total']}", end='', flush=True)
-                            print(f" - Completed: {chunk['completed']}", end='\n', flush=True)
-                        else:
-                            print()
         except requests.exceptions.RequestException as e:
-            print(f"An error occurred: {e}")
+            raise HTTPException(status_code=500, detail=f"An error occurred: {e}")
 
-    # List models that are available locally.
     def list_models(self):
         try:
             response = requests.get(f"{BASE_URL}/api/tags")
             response.raise_for_status()
             data = response.json()
-            models = data.get('models', [])
-            print("available models \n",models)
-            return models
+            return data.get('models', [])
 
         except requests.exceptions.RequestException as e:
-            print(f"An error occurred: {e}")
-            return None
+            raise HTTPException(status_code=500, detail=f"An error occurred: {e}")
 
-    # Copy a model. Creates a model with another name from an existing model.
     def copy_model(self, source, destination):
         try:
-            # Create the JSON payload
-            payload = {
-                "source": source,
-                "destination": destination
-            }
-            
+            payload = {"source": source, "destination": destination}
             response = requests.post(f"{BASE_URL}/api/copy", json=payload)
             response.raise_for_status()
-            
-            # If the request was successful, return a message indicating that the copy was successful
             return "Copy successful"
 
         except requests.exceptions.RequestException as e:
-            print(f"An error occurred: {e}")
-            return None
+            raise HTTPException(status_code=500, detail=f"An error occurred: {e}")
 
-    # Delete a model and its data.
     def delete_model(self, model_name):
         try:
-            url = f"{BASE_URL}/api/delete"
             payload = {"name": model_name}
-            response = requests.delete(url, json=payload)
+            response = requests.delete(f"{BASE_URL}/api/delete", json=payload)
             response.raise_for_status()
             return "Delete successful"
-        
+
         except requests.exceptions.RequestException as e:
-            print(f"An error occurred: {e}")
-            return None
+            raise HTTPException(status_code=500, detail=f"An error occurred: {e}")
 
     # Show info about a model.
     def show_model(self, model_name):
@@ -196,6 +130,7 @@ class OllamaRepository:
         except requests.exceptions.RequestException as e:
             print(f"An error occurred: {e}")
             return None
+        
 
     def heartbeat(self):
         try:
--- IngestionService/app/services/ollama_service.py
+++ IngestionService/app/services/ollama_service.py
@@ -1,20 +1,48 @@
 from app.repositories.ollama_repository import OllamaRepository
+from fastapi import HTTPException, status
 
 class OllamaService:
     def __init__(self):
         self.ollama_repository = OllamaRepository()
 
     def generate_text(self, model_name, prompt, system=None, template=None, context=None, options=None):
-        return self.ollama_repository.generate_text(model_name, prompt, system, template, context, options)
+        models = self.list_models()
 
+        # Check if the specified model_name exists in the list of models
+        if any(model['name'] == model_name for model in models):
+            # Pull the model
+            return self.ollama_repository.generate_text(model_name, prompt, system, template, context, options)
+        
+        else:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Model '{model_name}' doesn't exist, please specify the tag or change the model")
+
+        
     def create_model(self, model_name, model_path=None):
         return self.ollama_repository.create_model(model_name, model_path)
 
-    def pull_model(self, model_name, insecure=False):
-        return self.ollama_repository.pull_model(model_name, insecure)
+    def pull_model(self, model_name: str, insecure: bool = False):
+        models = self.list_models()
+
+        # Check if the specified model_name exists in the list of models
+        if all(model['name'] != model_name for model in models):
+            # Pull the model
+            return self.ollama_repository.pull_model(model_name, insecure)
+        
+        else:
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"Model '{model_name}' already exists")
+
 
     def push_model(self, model_name, insecure=False):
-        return self.ollama_repository.push_model(model_name, insecure)
+        models = self.list_models()
+
+        # Check if the specified model_name exists in the list of models
+        if all(model['name'] != model_name for model in models):
+            # Pull the model
+            return self.ollama_repository.push_model(model_name, insecure)
+        
+        else:
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"Model '{model_name}' already exists")
+        
 
     def list_models(self):
         return self.ollama_repository.list_models()
@@ -23,10 +51,28 @@ class OllamaService:
         return self.ollama_repository.copy_model(source, destination)
 
     def delete_model(self, model_name):
-        return self.ollama_repository.delete_model(model_name)
+        models = self.list_models()
+
+        # Check if the specified model_name exists in the list of models
+        if any(model['name'] == model_name for model in models):
+            # Pull the model
+            return self.ollama_repository.delete_model(model_name)
+        
+        else:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Model '{model_name}' doesn't exist, please specify the tag or change the model")
+
 
     def show_model(self, model_name):
-        return self.ollama_repository.show_model(model_name)
+        models = self.list_models()
+
+        # Check if the specified model_name exists in the list of models
+        if any(model['name'] == model_name for model in models):
+            # Pull the model
+            return self.ollama_repository.show_model(model_name)
+        
+        else:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Model '{model_name}' doesn't exist, please specify the tag or change the model")
+        
 
     def check_ollama_health(self):
         return self.ollama_repository.heartbeat()
